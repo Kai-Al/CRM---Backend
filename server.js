8f271378ba6b0a39ff38f101906397a7caba6234
Kai-Al <Kai.albornoz@udea.edu.co>
2022-09-27 14:25:55 -0500

    Inicialización del repositorio.

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const cors = require("cors");
+require ('dotenv').config();
+
+const app = express();
+
+const db = require("./models");
+const Role = db.role;
+
+
+db.mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+var corsOptions = {
+  origin: "*",
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CRM - Backend" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'moderator' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
